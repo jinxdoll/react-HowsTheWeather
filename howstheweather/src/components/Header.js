58f3dd5a911aee96d8eda3e5095192e981739029
amatt <amattson84@gmail.com>
2021-04-27 22:51:06 -0700

    commit

--- howstheweather/src/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default () => (
-  <nav className="navbar">
-    <h2 className="navbar-title">Weather App</h2>
-  </nav>
-);
--- howstheweather/src/constants.js
+++
@@ -1,3 +0,0 @@
-const API_KEY = "fcfffdc78caadbeba241917135e27cb7";
-
-export { API_KEY };
